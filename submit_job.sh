33c0da9a0ea4962fc7026e4ebaaa7d16fb4633cc
tsu0516 <TongSu2026@u.northwestern.edu>
2025-06-02 14:31:45 -0500

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=dehualiu
+#SBATCH --job-name=1
 #SBATCH --account=p32696
 #SBATCH --partition=gengpu
 #SBATCH --gres=gpu:h100:1
 #SBATCH --mem=128G
 #SBATCH --time=12:00:00
-#SBATCH --output=job_output_M3S_ACom.out
-#SBATCH --error=error_M3S_ACom.out
+#SBATCH --output=job_output.out
+#SBATCH --error=error.out
 
 module purge
 
@@ -17,30 +17,8 @@ source /home/mru0861/miniconda3/etc/profile.d/conda.sh
 conda activate /home/mru0861/miniconda3/envs/DR_GST
 
 # 进入代码目录
-cd /home/mru0861/projects/DR-GST-ST/baseline
+cd /home/mru0861/projects/DR-GST-ST
 
 # 运行主程序
-#python main.py --dataset CaCS --model GCN --labelrate 3 --s 3 --t 0.5 --b 0.1 --drop_method dropout --droprate 0.3
-# for rate in 3 5 10 20
-# do
-#   echo "Running labelrate=$rate"
-#   python main.py --dataset CaCS --model GCN --labelrate $rate --drop_method dropout --droprate 0.3
-# done
+python main.py --dataset CaCS --model GCN --labelrate 3 --s 3 --t 0.5 --b 0.1 --drop_method dropout --droprate 0.3
 
-# for rate in 3 5 10 20
-# do
-#  echo "Running labelrate=$rate"
-#  python main.py --dataset CaCS --model GCN --labelrate $rate --drop_method dropout --droprate 0.3
-# done
-
-# for rate in 3
-# do
-#     echo "Running labelrate=$rate"
-#     python m3s.py --dataset Pubmed --model GCN --labelrate $rate
-# done
-
-for rate in 3 5 10 20
-do
-    echo "Running labelrate=$rate"
-    python m3s.py --dataset ACom --model GCN --labelrate $rate
-done
