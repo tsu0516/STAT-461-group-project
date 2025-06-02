9e90b209a7377223e037e95cac07323e8b2913e9
tsu0516 <TongSu2026@u.northwestern.edu>
2025-06-02 14:56:21 -0500

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -10,15 +10,11 @@
 
 module purge
 
-# 加载 conda 初始化脚本（路径需根据你环境调整）
 source /home/mru0861/miniconda3/etc/profile.d/conda.sh
 
-# 激活你的环境
 conda activate /home/mru0861/miniconda3/envs/DR_GST
 
-# 进入代码目录
 cd /home/mru0861/projects/DR-GST-ST
 
-# 运行主程序
 python main.py --dataset CaCS --model GCN --labelrate 3 --s 3 --t 0.5 --b 0.1 --drop_method dropout --droprate 0.3
 
